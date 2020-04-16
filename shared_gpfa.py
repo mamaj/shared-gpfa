8551cf394bd1ad660c2a8cf0aac7328e11aad5b0
ebrahimi-mr <mamareza.e@gmail.com>
2020-04-16 16:09:05 -0400

    initialize X & W with FA/LS in fit and add_video.

--- shared_gpfa.py
+++ shared_gpfa.py
@@ -1,4 +1,6 @@
 import datetime
+import math
+import warnings
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -7,7 +9,7 @@ import pandas as pd
 import tensorflow as tf
 import tensorflow_probability as tfp
 from tqdm import tqdm
-import math
+from sklearn.decomposition import FactorAnalysis
 
 tfd = tfp.distributions
 tfb = tfp.bijectors
@@ -15,7 +17,7 @@ dtype = np.float32
 
 class SharedGpfa:
 
-    def __init__(self, m, q, p, latent_noise_var=1e-4, dtype=np.float32, reg=0.):
+    def __init__(self, m, q, p, latent_noise_var=1e-4, dtype=np.float32, reg=0., fa_init=False):
         self.m = m
         self.q = q
         self.p = p
@@ -26,6 +28,7 @@ class SharedGpfa:
         self.dtype = dtype
         self.vars = dict()
         self.joint = []
+        self.fa_init = fa_init
 
     def init_vars(self):
         self.check_model_fit()
@@ -35,7 +38,7 @@ class SharedGpfa:
 
         constrain_positive = tfb.Shift(np.finfo(np.float32).tiny)(tfb.Exp())
 
-        self.vars['w'] = tf.Variable(init_norm((self.m, self.q, self.p)), name='w')
+        self.vars['w'] = tf.Variable(init_uniform((self.m, self.q, self.p)), name='w')
         self.vars['length_scale'] = tfp.util.TransformedVariable(init_log((self.p)), constrain_positive, name='length_scale')
         self.vars['subject_noise_scale'] = tfp.util.TransformedVariable(init_log(self.m), constrain_positive, name='subject_noise_scale')
         self.vars['roi_noise_scale'] = tfp.util.TransformedVariable(init_log(self.q), constrain_positive, name='roi_noise_scale')
@@ -43,7 +46,6 @@ class SharedGpfa:
         for i, t in enumerate(self.t):
             x = tf.Variable(init_norm((self.p, t)), name='x{i}')
             self.vars['x'].append(x)
-
 
     def init_joint(self):
         self.check_model_fit()
@@ -97,6 +99,8 @@ class SharedGpfa:
         train_data = self.add_batch(train_data)
         self.t = [s.shape[-1] for s in train_data]
         self.init_vars()
+        if self.fa_init:
+            self.assign_fa(train_data)
         self.init_joint()
         
         opt = tf.optimizers.Adam(learning_rate=learning_rate)
@@ -120,25 +124,33 @@ class SharedGpfa:
         return l
 
 
-    def add_video(self, obs, n_iters=1e3, learning_rate=0.04, subs=Ellipsis, name='new_x', tensorboard=False, **kwargs):
+    def add_video(self, obs, n_iters=1e3, learning_rate=0.04, subs=Ellipsis, name='new_x', tensorboard=False, fa_init=None, **kwargs):
 
         if subs is None: subs = range(self.m)
+        if fa_init is None: fa_init = self.fa_init
+
         obs = self.add_batch(obs)
         obs_subs = []
         for s in obs:
             obs_subs.append(s[subs])
         t = [s.shape[-1] for s in obs_subs]
-
-
         new_joint = [self.create_joint(_t, subs=subs) for _t in t]
-        init = lambda size: np.random.normal(size=size).astype(self.dtype)
+        
+        if fa_init:
+            xlist_pinv = self.least_square(obs)
+        else:
+            init = lambda size: np.random.normal(size=size).astype(self.dtype)
+
         xlist = []
         for i, _t in enumerate(t):
-            x = tf.Variable(init((self.p, _t)), name=name+f'_{i}')
+            if fa_init:
+                x_init = xlist_pinv[i]
+            else:
+                x_init = init((self.p, _t))
+            x = tf.Variable(x_init, name=name+f'_{i}', dtype=dtype)
             xlist.append(x)
 
         if tensorboard: self.setup_tfboard()
-
         opt = tf.optimizers.Adam(learning_rate=learning_rate)
 
         @tf.function
@@ -155,7 +167,7 @@ class SharedGpfa:
             opt.minimize(loss, xlist)
 
         # l = []
-        for epoch in tqdm(range(int(n_iters)), **kwargs):
+        for epoch in range(int(n_iters)):
             train_step()
             if tensorboard: 
                 self.update_tfsummary(loss(), epoch)
@@ -163,10 +175,8 @@ class SharedGpfa:
                     for i, x in enumerate(xlist):
                         tf.summary.histogram(f'xlist{i}_0', x[0], step=epoch)
                         tf.summary.histogram(f'xlist{i}_1', x[1], step=epoch)
-
             # l.append(loss())
         return xlist, recon_loss()
-
 
 
     def add_subject(self, obs, experiment, n_iters=1e3, learning_rate=0.04, name='w_new'):
@@ -187,6 +197,47 @@ class SharedGpfa:
         for i in range(int(n_iters)):
             train_step()
         return w, loss()
+
+
+    def factor_analysis(self, data, **kwargs):
+        
+        fa = FactorAnalysis(n_components=self.p, random_state=0, **kwargs)
+
+        y = np.concatenate(data, -1)
+        y = np.concatenate(y, 0)
+        y = y.transpose()
+
+        x = fa.fit_transform(y)
+
+        x = x.transpose()
+        x = np.array_split(x, [data[0].shape[-1]], axis=-1)
+        w = fa.components_.transpose()
+        w = np.split(w, data[0].shape[0])
+        w = np.stack(w, axis=0)
+
+        return x, w
+
+
+    def assign_fa(self, data):
+        if sum(d.shape[-1] for d in data) < self.p:
+            warnings.warn("p > t, cannot use FA for initialization.")
+            return
+        
+        x_fa, w_fa = self.factor_analysis(data)
+        for _x, _x_fa in zip(self.vars['x'], x_fa):
+            _x.assign(_x_fa)
+        self.vars['w'].assign(w_fa)
+        
+
+    def least_square(self, data):
+        w = self.vars['w'].numpy()
+        w = np.concatenate(w, axis=0)
+        w_pinv = np.linalg.pinv(w)
+        xlist = []
+        for y in data:
+            x_hat = w_pinv @ np.concatenate(y, axis=0)
+            xlist.append(x_hat)
+        return xlist
 
 
     def setup_tfboard(self):
