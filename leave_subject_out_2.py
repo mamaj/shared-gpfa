eba58890e34e9942439a02ea003057f4b8efe991
ebrahimi-mr <mamareza.e@gmail.com>
2020-04-16 16:09:55 -0400

    completed implimentation

--- leave_subject_out_2.py
+++ leave_subject_out_2.py
@@ -13,89 +13,53 @@ import math
 
 tf.get_logger().setLevel('INFO')
 
-# Some Training videos
 # Learn the model parameters (W, \tau) on training videos
 # On the test video, leave one subject out, 
 # learn the X for the test videos using all-but-one subject
 # and find the reconstruction error for held-out subject.
 
 
-train = [load_ea_data(vid=v)[0] for v in [0, 1, 2, 3, 4, 5]]
-test = [load_ea_data(vid=v)[0] for v in [6, 7, 8]]
+train = [load_ea_data(vid=v)[0] for v in [0, 1]]
+test = [load_ea_data(vid=v)[0] for v in [2]]
 m, q, _ = train[0].shape
 
-p_range = np.arange(5, 200, 5)
+# p_range = np.arange(5, 200, 5)
+p_range = [160]
 
 error = []
 lscales = []
 
-ncols = 4
-nrows = math.ceil(p_range.size / ncols)
-fig, axs = plt.subplots(nrows, ncols, figsize=(ncols*5, nrows*5))
-fig_bar, ax_bar = plt.subplots()
-plt.ion()
-plt.show()
-
-for i, p in enumerate(tqdm(p_range, desc='total', ncols=100)):
+loss = []
+for p in tqdm(p_range, desc='total', ncols=100):
     # train the model
-    model = SharedGpfa(m, q, p, reg=0.)
+    model = SharedGpfa(m, q, p, reg=0., fa_init=True)
     model.fit(
         train_data=train, 
-        n_iters=1200, 
-        learning_rate=0.075, 
+        n_iters=100, 
+        learning_rate=0.1, 
         tensorboard=False,
-        desc=f'p = {p}',
+        desc=f'Training: p = {p}',
         ncols=100
     )
 
     # test the model
+    err = []
+    # for test_sub in tqdm(range(m), desc=f'p = {p}', ncols=100):
+    for test_sub in tqdm(range(80), desc=f'p = {p}', ncols=100):
+        train_subs = subs = np.arange(m) != test_sub
+        x_test = model.add_video(
+            test,
+            subs=train_subs,
+            fa_init=True,
+            n_iters=100,
+            learning_rate=0.1)[0][0]
+        y_hat = (model.vars['w'][test_sub] @ x_test).numpy()
+        y = test[0][test_sub]
+        _err = np.sum((y - y_hat) ** 2)
+        err.append(_err)
+ 
+    loss.append(np.mean(err))
+    # np.save('loss', loss)
 
-
-
-    x, recon_error = model.add_video(test)
-    # recon_error = tf.reduce_sum((test - model.vars['w'] @ x) ** 2)
-    error.append(recon_error)
-    np.save('error', error)
-    np.save('lscale', lscales)
-    ax_bar.plot(p_range[:i+1], error, marker='.')
-    ax_bar.set_title(f'p = {p}')
-
-    plt.draw()
-    plt.pause(0.05)
-
-plt.show(block=True)
-
-
-
-
-
-
-
-data, subdf, score = load_ea_data(vid=0)
-m, q, t = data.shape
-
-n_splits = 5
-p_range = np.arange(1, 200, 5)
-
-kf = KFold(n_splits=n_splits, shuffle=True, random_state=1)
-
-error = []
-for p in tqdm(p_range):
-    print(f'started to evaluate p = {p}')
-    error.append([])
-    for train, test in tqdm(kf.split(data), total=n_splits):
-        model = SharedGpfa(len(train), q, p, t)
-        model.fit(
-            train_data=data[train], 
-            n_iters=1200, 
-            learning_rate=0.075, 
-            tensorboard=False
-        )
-        _, rec_error = model.add_subject(data[test])
-        error[-1].append(rec_error)
-    np.save('error', np.array(error))
-
-error = np.array(error)
-np.save('error', error)
-plt.plot(p_range, error.mean(-1), marker='.')
+plt.plot(p_range, loss)
 plt.show()
