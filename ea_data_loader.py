806724d1277a05648994419bb055a54154dfc485
Reza Ebrahimi <rebrahimi@com02.camhres.ca>
2020-04-24 19:13:52 -0400

    added get_atlas_ticks, get_fsavg_map

--- ea_data_loader.py
+++ ea_data_loader.py
@@ -6,15 +6,8 @@ import pandas as pd
 from scipy import stats
 
 
-def load_ea_data(vid, platform='win', normalize=True, filter_subs=True, circle_quantile=0.01, fdmean=0.5, fdperc=50):
-    if platform == 'reza_wsl':
-        data_path = Path('/mnt/c/Users/mamar/OneDrive - University of Toronto/Thesis/Brain Network/SPINS/SPINS_ciftify/schaefer_ea')
-    elif platform == 'reza_win':
-        data_path = PureWindowsPath(r"C:\Users\mamar\OneDrive - University of Toronto\Thesis\Brain Network\SPINS\SPINS_ciftify\schaefer_ea")
-    elif platform == 'camh':
-        data_path = Path('/projects/rebrahimi/GSP/SPINS_ciftify/schaefer_ea/')
-
-
+def load_ea_data(vid, platform='reza_win', normalize=True, filter_subs=True, circle_quantile=0.01, fdmean=0.5, fdperc=50):
+    data_path = get_data_path(platform)
     subdf = pd.read_csv(data_path / 'subjects_df.csv')
     vid_dur = pd.read_csv(data_path / 'vid_dur.csv').to_numpy()[0]
     
@@ -52,3 +45,30 @@ def get_filtered_subs(subdf, circle_quantile, fdmean, fdperc):
     fdperc_filter = subdf.filter(regex='fd_perc_run').ge(fdperc).any(axis=1)
 
     return (score_filter | fdmean_filter | fdperc_filter)
+
+def get_atlas_ticks(platform='reza_win'):
+    data_path = get_data_path(platform)
+    yeo = pd.read_csv(data_path / 'yeo17labels.csv')
+    label_ticks = np.where(yeo.region != yeo.region.shift())[0]
+    label_names = (yeo.hemi[label_ticks] + ' ' + yeo.region[label_ticks]).to_numpy()
+    return yeo, label_ticks, label_names
+    
+def get_data_path(platform='reza_win'):
+    if platform == 'reza_wsl':
+        data_path = Path('/mnt/c/Users/mamar/OneDrive - University of Toronto/Thesis/Brain Network/SPINS/SPINS_ciftify/schaefer_ea')
+    elif platform == 'reza_win':
+        data_path = PureWindowsPath(r"C:\Users\mamar\OneDrive - University of Toronto\Thesis\Brain Network\SPINS\SPINS_ciftify\schaefer_ea")
+    elif platform == 'camh':
+        data_path = Path('/projects/rebrahimi/GSP/SPINS_ciftify/schaefer_ea/')
+    else:
+        data_path = None
+    return data_path
+
+def get_fsavg_map(platform='camh'):
+    if platform == 'camh':
+        sch2fsavg = ['/projects/rebrahimi/GSP/SPINS_fMRIprep/Parcellations/FreeSurfer5.3/fsaverage5/label/lh.Schaefer2018_400Parcels_17Networks_order.annot',
+                     '/projects/rebrahimi/GSP/SPINS_fMRIprep/Parcellations/FreeSurfer5.3/fsaverage5/label/rh.Schaefer2018_400Parcels_17Networks_order.annot'
+                    ]
+    else:
+        sch2fsavg = None
+    return sch2fsavg
